3678f51a3eeb546c1e593dfd0e5b2cb1c8277f33
Martin Alex Okello <martin.okello@gmail.com>
2022-09-24 14:08:58 +0100

    Update IHttpClient.cs

--- BLG.Business/Interfaces/IHttpClient.cs
+++ BLG.Business/Interfaces/IHttpClient.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
 
+//Interface contract to create calls to other Apis using HTTPClient
 namespace BLG.Business.Interfaces
 {
     public interface IHttpClient
